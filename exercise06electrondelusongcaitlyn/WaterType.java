baeda35285b84a48caee812ffacfbdcdabaeab98
caitlyndl <96188120+caitlyndl@users.noreply.github.com>
2022-11-15 21:32:40 +0800

    Update WaterType.java

--- exercise06electrondelusongcaitlyn/WaterType.java
+++ exercise06electrondelusongcaitlyn/WaterType.java
@@ -14,7 +14,12 @@ public class WaterType extends Monster {
     super(name, "water", "fire", "grass", hp, base);
      atk *= 0.7;
      def *= 1.3;
-     
+  }
+  
+  @Override
+    public void special() {
+        def += 2;
+        hp -= maxHP*0.1;
   }
    
 }
